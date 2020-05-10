256b93b5aeba8c7960c07eaf8699b32510311a71
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2020-05-09 18:24:21 -0700

    Implement articles

--- property.go
+++ property.go
@@ -1,10 +1,22 @@
 package tinker
 
+import "github.com/Henry-Sarabia/article"
+
 // Property describes a specific physical property such as material or shape.
 type Property struct {
 	Name      string
 	Attribute Attribute
 	Verb      string
+	Article   string
+}
+
+// checkArticle returns the appropriate article.
+func checkArticle(word string, countable bool) string {
+	if !countable {
+		return ""
+	}
+
+	return article.Indefinite(word)
 }
 
 // PropertyRecipe describes a physical property such as material or shape.
@@ -13,8 +25,10 @@ type PropertyRecipe struct {
 	Frequency      float64  `json:"frequency"`
 	AttributeNames []string `json:"attribute_names"`
 	VerbNames      []string `json:"verb_names"`
+	Countable      bool     `json:"countable"`
 }
 
+// property creates a Property according the the PropertyRecipe.
 func (p *PropertyRecipe) property(atbs map[string]AttributeRecipe, verbs map[string]Verb) (Property, error) {
 	n := randString(p.AttributeNames)
 	atb, err := atbs[n].attribute(atbs)
@@ -27,5 +41,6 @@ func (p *PropertyRecipe) property(atbs map[string]AttributeRecipe, verbs map[str
 		Name:      p.Name,
 		Attribute: atb,
 		Verb:      verbs[v].RandVariant(),
+		Article:   checkArticle(atb.Description, p.Countable),
 	}, nil
 }
